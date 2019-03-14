cf29f4ca77ca22f1bc1465a4e085fbacf1566e36
Connor Kam-Cheong <connorkc42@gmail.com>
2019-03-14 16:46:46 +0000

    Add test for acceptance criteria, non-scoring strings

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,9 +7,21 @@ describe Scrabble do
       expect(scrabble.score).to be_a Integer
     end
 
-    it 'returns 0 for an empty string' do
-      scrabble = Scrabble.new('')
-      expect(scrabble.score).to eq 0
+    context 'non-scoring words' do
+      it 'returns 0 for an empty string' do
+        scrabble = Scrabble.new('')
+        expect(scrabble.score).to eq 0
+      end
+
+      it 'returns 0 for nil' do
+        scrabble = Scrabble.new(nil)
+        expect(scrabble.score).to eq 0
+      end
+
+      it 'returns 0 for " \t\n"' do
+        scrabble = Scrabble.new(' \t\n')
+        expect(scrabble.score).to eq 0
+      end
     end
 
     it 'returns 1 for "a"' do
@@ -19,7 +31,7 @@ describe Scrabble do
 
     it 'returns 4 for "f"' do
       scrabble = Scrabble.new('f')
-      expect(scrabble.score).to eq 4 
+      expect(scrabble.score).to eq 4
     end
   end
 end
