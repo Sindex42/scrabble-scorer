7fda66ec43a30c4ff4f8032fa54e171ca50e1957
Connor Kam-Cheong <connorkc42@gmail.com>
2019-03-14 16:43:51 +0000

    #score returns 4 for 'f'

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,12 +1,18 @@
 class Scrabble
-  attr_reader :word
+  attr_reader :word, :score
 
   def initialize(word)
     @word = word
+    @score = {
+      'A' => 1,
+      'F' => 4
+    }
   end
 
   def score
     return 1 if word == 'a'
+    return 4 if word == 'f'
+
     0
   end
 end
