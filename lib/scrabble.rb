dbbb1fd1aa3bf4658c0868098ac9dad83ce7481f
Connor Kam-Cheong <connorkc42@gmail.com>
2019-03-15 14:29:35 +0000

    Add full score table for letters

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,17 +1,18 @@
 class Scrabble
-  attr_reader :word, :letters, :letter_score
+  attr_reader :word, :letters, :score_table
 
   def initialize(word)
     @word = word
     @letters = word.upcase.split('') if word.is_a?(String)
     @word_score = 0
-    @letter_score = {
-      'A' => 1,
-      'S' => 1,
-      'T' => 1,
-      'R' => 1,
-      'E' => 1,
-      'F' => 4
+    @score_table = {
+      1 => %w[A E I O U L N R S T],
+      2 => %w[D G],
+      3 => %w[B C M P],
+      4 => %w[F H V W Y],
+      5 => %w[K],
+      8 => %w[J X],
+      10 => %w[Q Z]
     }
   end
 
@@ -19,13 +20,15 @@ class Scrabble
     return 0 unless word.is_a?(String)
 
     letters.map do |letter|
-      if letter_score.include?(letter)
-        @word_score += letter_score[letter]
-      else
-        return 0
+      return 0 unless /[A-Z]/.match?(letter)
+
+      score_table.map do |letter_value, letter_group|
+        if letter_group.include?(letter)
+          @word_score += letter_value
+        end
       end
     end
 
-    word_score
+    @word_score
   end
 end
